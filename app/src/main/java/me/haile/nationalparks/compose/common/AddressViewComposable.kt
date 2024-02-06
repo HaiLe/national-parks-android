0e5a223e697108f98c9e957d932ae5dcb26b3d78
Hai Le <honghaile@gmail.com>
2024-02-06 09:28:16 -0800

    remove unused imports

--- app/src/main/java/me/haile/nationalparks/compose/common/AddressViewComposable.kt
+++ app/src/main/java/me/haile/nationalparks/compose/common/AddressViewComposable.kt
@@ -1,14 +1,7 @@
 package me.haile.nationalparks.compose.common
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import me.haile.nationalparks.data.Address
 
 @Composable
@@ -25,7 +18,7 @@ fun AddressList(addresses: List<Address>) {
 fun AddressItem(address: Address) {
     Column() {
         StandardText(address.line1)
-        StandardText(text = "${address.city}, ${address.stateCode} ${address.postalCode}")
+        StandardText(text = "${address.city}, ${address.stateCode} ${address.postalCode}, ${address.countryCode}")
     }
 }
 
@@ -34,7 +27,6 @@ fun AddressItem(address: Address) {
 fun DisplayAddresses() {
     val sampleAddresses = listOf(
         Address("12345", "New York", "NY", "USA", "NY", "123 Main St", "Home", "Apt 2", "Floor 3"),
-        // Add more addresses here
     )
 
     AddressList(sampleAddresses)
