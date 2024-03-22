e3c7aad825351f4386cb0877d458a9f56718130d
Hai Le <honghaile@gmail.com>
2024-03-22 10:37:21 -0700

    add lego card

--- app/src/main/java/me/haile/nationalparks/compose/lego/LegoCardScreen.kt
+++ app/src/main/java/me/haile/nationalparks/compose/lego/LegoCardScreen.kt
@@ -1,13 +1,20 @@
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,7 +24,10 @@ import me.haile.nationalparks.compose.common.Separator
 @Composable
 fun LegoCardScreen() {
 
-    Column {
+    Column (Modifier.padding(16.dp)) {
+
+        Separator()
+
         Card() {
             Text(text = "Hello, world!")
         }
@@ -33,6 +43,43 @@ fun LegoCardScreen() {
         ) {
             Text(
                 text = "Filled content",
+                modifier = Modifier
+                    .padding(16.dp),
+                textAlign = TextAlign.Center,
+            )
+        }
+
+        Separator()
+
+        OutlinedCard(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface,
+            ),
+            border = BorderStroke(1.dp, Color.Black),
+            modifier = Modifier.fillMaxWidth().size(width = 240.dp, height = 100.dp).clip(
+                RoundedCornerShape(10.dp)
+            )
+
+        ) {
+            Text(
+                text = "Outlined",
+                modifier = Modifier
+                    .padding(16.dp),
+                textAlign = TextAlign.Center,
+            )
+        }
+
+        Separator()
+
+        ElevatedCard(
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 6.dp
+            ),
+            modifier = Modifier
+                .size(width = 240.dp, height = 100.dp)
+        ) {
+            Text(
+                text = "Elevated",
                 modifier = Modifier
                     .padding(16.dp),
                 textAlign = TextAlign.Center,
